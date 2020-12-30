d5d6110f7aa964c64b4f1bb7339fdf1c9ee07231
ciara-picou <ciarapicou@att.net>
2020-12-30 11:38:16 -0600

    debugged steps misalignment

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function update() {
       circle.classList.remove("active");
     }
   });
-  const actives = document.querySelectorAll("active");
+  const actives = document.querySelectorAll(".active");
 
   progress.style.width =
     ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
